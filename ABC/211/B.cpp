60ec5410914678998efdb92b126acbfbdab2457c
Yuulis <yuulis2005@gmail.com>
2021-08-22 00:30:43 +0900

    [Added] ABC 081A, B, 086A, 107B, 211A, B, 215A, ABS practiceA

---
+++ ABC/211/B.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF 1e+9;
+
+int main() {
+    int N;
+    cin >> N;
+
+    
+}
---
+++ ABS/practiceA.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF 1e+9;
+
+int main() {
+    int a, b, c;
+    string s;
+    cin >> a >> b >> c >> s;
+
+    cout << a + b + c << ' ' << s << endl;
+}
