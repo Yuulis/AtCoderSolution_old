2cd8e64305009e30a94e35c5155484cf9b034836
Yuulis <yuulis2005@gmail.com>
2021-09-04 23:05:07 +0900

    [Added] ABC 217A, B, C

---
+++ ABC/217/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using Graph = vector<vector<int>>;
+typedef long long ll;
+
+#define INF 1e+9;
+#define MOD 1000000007;
+
+int main() {
+    string S, T;
+    cin >> S >> T;
+
+    if (S < T)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
