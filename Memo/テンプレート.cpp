67b83c6c4137013edf066d9dbae0a2239d25fc4c
Yuulis <yuulis2005@gmail.com>
2021-09-17 13:26:31 +0900

    [Added] ABC 186A, B, C

--- Memo/テンプレート.cpp
+++ Memo/テンプレート.cpp
@@ -10,12 +10,19 @@ typedef long long ll;
 #define MOD 1000000007;
 
 int main() {
-    int N;
-    cin >> N;
+    int N, M;
+    cin >> N >> M;
 
     vector<int> A(N);
     for (int i = 0; i < N; i++) cin >> A[i];
 
     vector<pair<int, int>> B(N);
     for (int i = 0; i < N; i++) cin >> B[i].first >> B[i].second;
+
+    vector<vector<int>> C(M, vector<int>(N));
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> C[i][j];
+        }
+    }
 }
