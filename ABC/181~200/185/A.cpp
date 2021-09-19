b84feb184139f1c5a084f01b2a50c61a58215f4c
Yuulis <yuulis2005@gmail.com>
2021-09-19 09:51:48 +0900

    [Added] ABC 185A, B

---
+++ ABC/181~200/185/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+
+using namespace std;
+using Graph = vector<vector<int>>;
+
+typedef long long ll;
+
+#define INF 1e+9;
+#define MOD 1000000007;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    cout << min(a, min(b, min(c, d))) << endl;
+}
