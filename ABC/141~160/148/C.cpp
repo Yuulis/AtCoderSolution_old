971e3823a5b597c63c3055cfb4f02f98673d087f
Yuulis <yuulis2005@gmail.com>
2021-11-18 12:53:40 +0900

    [Added] ABC 148A, B, C, D

---
+++ ABC/141~160/148/C.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#include <map>
+#include <unordered_set>
+
+using namespace std;
+using Graph = vector<vector<int>>;
+
+typedef long long ll;
+
+#define INF 1e+9
+#define MOD 1000000007
+#define all(x) (x).begin(), (x).end()
+
+constexpr double PI = 3.14159265358979;
+
+template <class T>
+bool chmax(T &a, const T &b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+bool chmin(T &a, const T &b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+
+// ======================================== //
+
+int main() {
+    ll A, B;
+    cin >> A >> B;
+
+    cout << lcm(A, B) << endl;
+}
