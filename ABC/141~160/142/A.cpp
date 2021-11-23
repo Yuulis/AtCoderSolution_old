c13a7334831077bd281512617a59ad367ea10ada
Yuulis <yuulis2005@gmail.com>
2021-11-23 18:52:59 +0900

    [Added] ABC 142A, B, C

---
+++ ABC/141~160/142/A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#include <map>
+#include <unordered_set>
+
+using namespace std;
+using Graph = vector<vector<int>>;
+
+typedef long long ll;
+
+#define INF 1e+9
+#define INFL 1LL << 60
+#define MOD 1000000007
+#define all(x) (x).begin(), (x).end()
+
+constexpr double PI = 3.14159265358979;
+
+template <class T>
+bool chmax(T &a, const T &b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+bool chmin(T &a, const T &b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+
+// ======================================== //
+
+int main() {
+    int N;
+    cin >> N;
+
+    int cnt = 0;
+    for (int i = 1; i <= N; i++) {
+        if (i % 2 != 0) cnt++;
+    }
+
+    cout << fixed << setprecision(10) << (double)cnt / N << endl;
+}
