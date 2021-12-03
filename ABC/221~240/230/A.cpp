06275e480ef4e23e122b205e240abf878dfb1fbc
Yuulis <yuulis2005@gmail.com>
2021-12-03 23:22:47 +0900

    [Added] ABC 230A, B, C

---
+++ ABC/221~240/230/A.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#include <map>
+#include <unordered_set>
+
+using namespace std;
+using Graph = vector<vector<int>>;
+
+typedef long long ll;
+
+#define INF 1e+9
+#define INFL 1LL << 60
+#define MOD 1000000007
+#define all(x) (x).begin(), (x).end()
+
+constexpr double PI = 3.14159265358979;
+
+template <class T>
+bool chmax(T &a, const T &b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+bool chmin(T &a, const T &b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+
+// ======================================== //
+
+int main() {
+    int N;
+    cin >> N;
+    string s;
+
+    if (N < 42) {
+        s = to_string(N);
+
+    } else {
+        s = to_string(N + 1);
+    }
+
+    if (N < 10)
+        cout << "AGC00" << s << endl;
+    else
+        cout << "AGC0" << s << endl;
+}
