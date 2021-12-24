9f7b141fa5871e9c1d160330a48eb5232357bdc2
Yuulis <yuulis2005@gmail.com>
2021-12-24 13:53:15 +0900

    [Added] ABC 132A, B, C Updated Libraries

--- Libraries/二分探索.cpp
+++ Libraries/二分探索.cpp
@@ -1,18 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-using Graph = vector<vector<int>>;
-typedef long long ll;
 
-#define INF 1e+9;
-#define MOD 1000000007;
+#define all(x) (x).begin(), (x).end()
+
+// cf : https://qiita.com/ganyariya/items/33f1326154b85db465c3
 
 int main() {
     int N;
-    cin >> N;
     vector<int> A(N);
-    for (int i = 0; i < N; i++) cin >> A.at(i);
 
-    // 二分探索
+    sort(all(A));  // ソート必須!
     if (binary_search(A.begin(), A.end(), 5)) {
         cout << "Found" << endl;  // 配列Aに5が入っている
     }
