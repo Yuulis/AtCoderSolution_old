2a34b885363a347196f7312db148e2bfae84be3a
Yuulis <yuulis2005@gmail.com>
2022-01-06 10:18:11 +0900

    Deleted same files.

--- Libraries/数字系/切り上げ除法.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-typedef long long ll;
-
-int RoundUpDivision(ll a, ll b) {
-    ll ans = (a + b - 1) / b;
-    return ans;
-}
--- Libraries/文字列系/文字を数字に変換.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    string S;
-
-    // 文字列Sの1文字目を数字aに変換
-    int a = int(S[0] - '0');
-}
