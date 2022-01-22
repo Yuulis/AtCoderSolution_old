755b275c27063395f7deeff0e1ef195d3762f749
Yuulis <yuulis2005@gmail.com>
2022-01-22 23:30:23 +0900

    [Added] ARC 133A

---
+++ AtCoder/ARC/133/A.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+using Graph = vector<vector<int>>;
+
+typedef long long ll;
+
+#define all(x) (x).begin(), (x).end()
+
+constexpr double PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+constexpr auto MOD = 1000000007;
+
+ll ceil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+// ======================================== //
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    int x = 0;
+    for (int i = 0; i < N; i++) {
+        if (x <= A[i]) x = A[i];
+        else break;
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (A[i] != x) cout << A[i] << " ";
+    }
+}
