8da6c58e26493697701833eb4ae221ddf86b792b
Yuulis <yuulis2005@gmail.com>
2022-02-10 17:45:22 +0900

    [Added] ABC 188C

---
+++ AtCoder/ABC/181~200/188/C.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <atcoder/modint.hpp>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using Graph = vector<vector<int>>;
+using mint = modint998244353;
+
+#define all(x) (x).begin(), (x).end()
+
+constexpr double PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+ll intCeil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+// ======================================== //
+
+int main() {
+    int N;
+    cin >> N;
+    vector<ll> A(1 << N);
+    for (ll i = 0; i < (1 << N); i++) cin >> A[i];
+
+    ll half = 1 << (N - 1);
+    ll max = max_element(all(A)) - A.begin();
+
+    auto start = max < half ? A.begin() + half : A.begin();
+
+    cout << (int)(max_element(start, start + half) - A.begin() + 1) << endl;
+}
