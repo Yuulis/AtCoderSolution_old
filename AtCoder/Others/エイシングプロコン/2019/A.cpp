7bca57533e41e35c05c2f6c0469b1a2abe7ee2b0
Yuulis <yuulis2005@gmail.com>
2022-02-11 00:52:21 +0900

    [Added] ABC 078B, エイジングプロコン-2019A

---
+++ AtCoder/Others/エイシングプロコン/2019/A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <atcoder/modint.hpp>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using Graph = vector<vector<int>>;
+using mint = modint998244353;
+
+#define all(x) (x).begin(), (x).end()
+
+constexpr double PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+ll intCeil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+// ======================================== //
+
+int main() {
+    int N, H, W;
+    cin >> N >> H >> W;
+
+    cout << (N - H + 1) * (N - W + 1) << endl;
+}
