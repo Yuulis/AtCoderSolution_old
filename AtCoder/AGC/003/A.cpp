5680bc90a01f5cdf5af27496cbbb60ffa79d6e91
Yuulis <yuulis2005@gmail.com>
2022-02-18 15:48:46 +0900

    [Added] APC 001A, ARC 007A, AGC 003A

---
+++ AtCoder/AGC/003/A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <atcoder/modint.hpp>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using Graph = vector<vector<int>>;
+using mint = modint998244353;
+
+#define all(x) (x).begin(), (x).end()
+
+constexpr double PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+ll intCeil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+mint modPow(ll x, ll n) { mint ans = 1; for (ll i = 0; i < n; i++) ans *= x; return ans; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+// ======================================== //
+
+int main() {
+    string S;
+    cin >> S;
+
+    if (S.find("N") != string::npos && S.find("S") == string::npos) cout << "No" << endl;
+    else if (S.find("S") != string::npos && S.find("N") == string::npos) cout << "No" << endl;
+    else if (S.find("E") != string::npos && S.find("W") == string::npos) cout << "No" << endl;
+    else if (S.find("W") != string::npos && S.find("E") == string::npos) cout << "No" << endl;
+    else cout << "Yes" << endl;
+}
---
+++ AtCoder/APC/001/A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <atcoder/modint.hpp>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using Graph = vector<vector<int>>;
+using mint = modint998244353;
+
+#define all(x) (x).begin(), (x).end()
+
+constexpr double PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+ll intCeil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+mint modPow(ll x, ll n) { mint ans = 1; for (ll i = 0; i < n; i++) ans *= x; return ans; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+// ======================================== //
+
+int main() {
+    ll X, Y;
+    cin >> X >> Y;
+
+    if (X == Y) cout << -1 << endl;
+    else if (X % Y == 0) cout << -1 << endl;
+    else cout << X << endl;
+}
