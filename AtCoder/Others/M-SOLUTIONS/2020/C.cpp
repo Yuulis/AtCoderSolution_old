c767bcbc2bdcb727dd9b521fb86be63c35465c2e
Yuulis <yuulis2005@gmail.com>
2022-02-23 10:48:36 +0900

    [Added] M-SOLUTIONS-2020 A

---
+++ AtCoder/Others/M-SOLUTIONS/2020/C.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <atcoder/modint.hpp>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using Graph = vector<vector<int>>;
+using mint = modint998244353;
+
+#define all(x) (x).begin(), (x).end()
+
+constexpr double PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+ll intCeil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+mint modPow(ll x, ll n) { mint ans = 1; for (ll i = 0; i < n; i++) ans *= x; return ans; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+// ======================================== //
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+    vector<int> A(N);
+    for (int i = 0; i < N; i++) cin >> A[i];
+
+    for (int i = K; i < N; i++) {
+        if (A[i - K] < A[i]) cout << "Yes" << endl;
+        else cout << "No" << endl;
+    }
+}
