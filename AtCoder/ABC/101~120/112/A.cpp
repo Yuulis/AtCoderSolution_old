272591d6f2ba832607b62ab9a9e7448a8da3038d
Yuulis <yuulis2005@gmail.com>
2022-03-28 00:03:12 +0900

    [Added] ABC 046A, 112A, AGC 046A, ARC 108A, 110A, 116A, Code-FESTIVAL-2016-qualB B, 天下一プロコン-2019 B

---
+++ AtCoder/ABC/101~120/112/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using lld = long double;
+using Graph = vector<vector<int>>;
+using mint = modint998244353;
+
+#define all(x) (x).begin(), (x).end()
+
+constexpr auto PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+ll intCeil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+mint modPow(ll x, ll n) { mint ans = 1; for (ll i = 0; i < n; i++) ans *= x; return ans; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+// ======================================== //
+
+int main() {
+    int N;
+    cin >> N;
+
+    if (N == 1) cout << "Hello World" << endl;
+    else {
+        int A, B;
+        cin >> A >> B;
+        cout << A + B << endl;
+    }
+}
