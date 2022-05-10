431e7e9af9bb03ba275f9acb72892d1b441d5d98
Yuulis <yuulis2005@gmail.com>
2022-05-10 23:14:12 +0900

    [Added] ABC 079-B, AGC 002-A

---
+++ AtCoder/AGC/002/A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using lld = long double;
+using Graph = vector<vector<int>>;
+using Pair = vector<pair<int, int>>;
+using mint = modint998244353;
+
+#define all(x) (x).begin(), (x).end()
+
+constexpr auto PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+ll intCeil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+mint modPow(ll x, ll n) { mint ans = 1; for (ll i = 0; i < n; i++) ans *= x; return ans; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+// ======================================== //
+
+int main() {
+    ll A, B;
+    cin >> A >> B;
+    
+    if (A == 0 || B == 0 || (A < 0 && B > 0)) cout << "Zero" << endl;
+    else if (A > 0 && B > 0) cout << "Positive" << endl;
+    else {
+        if ((B - A + 1) % 2 == 0) cout << "Positive" << endl;
+        else cout << "Negative" << endl;
+    }
+}
