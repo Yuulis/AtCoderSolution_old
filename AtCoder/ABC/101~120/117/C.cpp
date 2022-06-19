743d3f0b501fa214be4bc0d988a301736749aae6
Yuulis <yuulis2005@gmail.com>
2022-06-19 10:49:07 +0900

    [Added] ABC 117-C

---
+++ AtCoder/ABC/101~120/117/C.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using lld = long double;
+using Graph = vector<vector<int>>;
+using Pair = vector<pair<int, int>>;
+using mint = modint998244353;
+
+random_device rd;
+default_random_engine eng(rd());
+uniform_int_distribution<int> distr(0, 3);
+
+#define all(x) (x).begin(), (x).end()
+
+constexpr auto PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+ll ceil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+mint modPow(ll x, ll n) { mint ans = 1; for (ll i = 0; i < n; i++) ans *= x; return ans; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+// ======================================== //
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> X(M);
+    for (int i = 0; i < M; i++) cin >> X[i];
+    sort(all(X));
+
+    vector<int> diff;
+    for (int i = 0; i < M - 1; i++)
+    {
+        diff.push_back(X[i + 1] - X[i]);
+    }
+    sort(all(diff));
+
+    ll ans = 0;
+    int cnt = max(0, M - N);
+    for (int i = 0; i < cnt; i++)
+    {
+        ans += diff[i];
+    }
+
+    cout << ans << endl;
+}
