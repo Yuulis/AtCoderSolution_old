841c4c05e44ed9713ad1f7905b49734efb16f0b3
Yuulis <yuulis2005@gmail.com>
2022-07-07 21:25:13 +0900

    [Added] ABC-102-A

--- AtCoder/ABC/101~120/102/A.cpp
+++ AtCoder/ABC/101~120/102/A.cpp
@@ -31,27 +31,6 @@ template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; 
 int main() {
     int N;
     cin >> N;
-    vector<int> A(N);
-    for (int i = 0; i < N; i++) cin >> A[i];
-    cout << fixed << setprecision(10) << N << endl;
+    
+    cout << lcm(2, N) << endl;
 }
-
-int N, M;
-vector<vector<int>> C(M, vector<int>(N));
-for (int i = 0; i < M; i++) {
-    for (int j = 0; j < N; j++) {
-        cin >> C[i][j];
-    }
-}
-vector<vector<ll>> v;
-v.resize(N);
-for (int i = 0; i < N; i++) {
-    ll L;
-    cin >> L;
-
-    v[i].resize(L);
-    for (int j = 0; j < L; j++) {
-        cin >> v[i][j];
-    }
-}
-set<int> E;
