36aa1e221700d04ef40a1dccbfc878446bd6fadf
Yuulis <yuulis2005@gmail.com>
2022-07-16 12:59:20 +0900

    [Added] ABC-055-C

---
+++ AtCoder/ABC/041~060/055/C.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using lld = long double;
+using Graph = vector<vector<int>>;
+using mint = modint998244353;
+
+random_device rd;
+default_random_engine eng(rd());
+uniform_int_distribution<int> distr(0, 3);
+
+#define all(x) (x).begin(), (x).end()
+
+constexpr auto PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+typedef pair<int, int> Pair;
+
+ll ceil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+mint modPow(ll x, ll n) { mint ans = 1; for (ll i = 0; i < n; i++) ans *= x; return ans; }
+ll gcd(ll x, ll y) { if (x < y) swap(x, y); ll r; while (y > 0) { r = x % y; x = y; y = r; } return x; }
+ll lcm(ll x, ll y) { return ll(x / gcd(x, y)) * y; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+// ======================================== //
+
+int main() {
+    ll N, M;
+    cin >> N >> M;
+    
+    ll ans = min(N, M / 2);
+    M -= ans * 2;
+    ans += M / 4;
+
+    cout << ans << endl;
+}
