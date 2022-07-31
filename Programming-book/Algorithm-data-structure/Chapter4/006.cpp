dcb61871ab810ad6334134131218fbe50800dc61
Yuulis <yuulis2005@gmail.com>
2022-07-31 12:28:42 +0900

    [Added] ABC-114-C

---
+++ Programming-book/Algorithm-data-structure/Chapter4/006.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> v;
+
+bool func(int i, int w, const vector<int>& a) {
+    if (i == 0) {
+        if (w == 0) return true;
+        else return false;
+    }
+
+    if (v[i][w] != -1) return v[i][w];
+
+    if (func(i - 1, w, a)) return v[i][w] = 1;
+
+    if (func(i - 1, w - a[i - 1], a)) return v[i][w] = 1;
+
+    return v[i][w] = 0;
+}
+
+int main() {
+    int N, W;
+    cin >> N >> W;
+    vector<int> a(N);
+    for (int i = 0; i < N; ++i) cin >> a[i];
+
+    v.assign(N + 1, vector<int>(W + 1, -1));
+
+    if (func(N, W, a)) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
