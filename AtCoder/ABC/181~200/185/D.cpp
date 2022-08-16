afb27d6f4429e8a84299514a55a4465a93882483
Yuulis <yuulis2005@gmail.com>
2022-08-16 12:12:43 +0900

    [Added] ABC-185-D

---
+++ AtCoder/ABC/181~200/185/D.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define all(x) (x).begin(), (x).end()
+#define fix(x) fixed << setprecision(x)
+#define rep(i,start,end) for(int i=(start);i<(int)(end);i++)
+#define rrep(i,start,end) for(int i=((int)(start));i>=(end);i--)
+
+constexpr auto PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using lld = long double;
+using mint = modint1000000007;
+// using mint = modint998244353;
+using Pair = pair<int, int>;
+
+mt19937 mt{ random_device{}() };
+uniform_int_distribution<int> rd(0, 3);
+
+ll ceil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+mint modPow(ll x, ll n) { mint ans = 1; for (ll i = 0; i < n; i++) ans *= x; return ans; }
+ll gcd(ll x, ll y) { if (x < y) swap(x, y); ll r; while (y > 0) { r = x % y; x = y; y = r; } return x; }
+ll lcm(ll x, ll y) { return ll(x / gcd(x, y)) * y; }
+ll nCk(ll N, ll K) { if (K < 0 || N < K) return 0; ll ans = 1; for (ll i = 1; i <= K; i++) { ans *= N--; ans /= i; } return ans; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+// ======================================== //
+
+int main() {
+    ll N, M;
+    cin >> N >> M;
+    vector<ll> A(M);
+    rep(i, 0, M) cin >> A[i];
+
+    if (M == 0) {
+        cout << 1 << endl;
+        return 0;
+    }
+
+    sort(all(A));
+    vector<ll> x(M + 1);
+    x[0] = A[0] - 1;
+    rep(i, 1, M) x[i] = A[i] - A[i - 1] - 1;
+    x[M] = N - A[M - 1];
+
+    ll minv = INF;
+    rep(i, 0, M + 1) if (x[i] > 0 && x[i] < minv) minv = x[i];
+
+    if (minv == INF) {
+        cout << 0 << endl;
+        return 0;
+    }
+    else {
+        ll ans = 0;
+        rep(i, 0, M + 1) if (x[i] > 0) ans += (x[i] - 1) / minv + 1;
+
+        cout << ans << endl;
+    }
+
+}
