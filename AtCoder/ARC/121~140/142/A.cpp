9f1d0083d0f5ea7254ae667d345bb41c1eb6279b
Yuulis <yuulis2005@gmail.com>
2022-08-23 10:44:06 +0900

    [Added] ARC-142-A

---
+++ AtCoder/ARC/121~140/142/A.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define all(x) (x).begin(), (x).end()
+#define fix(x) fixed << setprecision(x)
+#define rep(i,start,end) for(int i=(start);i<(int)(end);i++)
+#define rrep(i,start,end) for(int i=((int)(start));i>=(end);i--)
+
+constexpr auto PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using lld = long double;
+using mint = modint1000000007;
+// using mint = modint998244353;
+using Pair = pair<int, int>;
+
+mt19937 mt{ random_device{}() };
+uniform_int_distribution<int> rd(0, 3);
+
+ll ceil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+mint modPow(ll x, ll n) { mint ans = 1; for (ll i = 0; i < n; i++) ans *= x; return ans; }
+ll gcd(ll x, ll y) { if (x < y) swap(x, y); ll r; while (y > 0) { r = x % y; x = y; y = r; } return x; }
+ll lcm(ll x, ll y) { return ll(x / gcd(x, y)) * y; }
+ll nCk(ll N, ll K) { if (K < 0 || N < K) return 0; ll ans = 1; for (ll i = 1; i <= K; i++) { ans *= N--; ans /= i; } return ans; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+// ======================================== //
+
+ll f(ll x) {
+    ll res = 0;
+    while (x)
+    {
+        res = res * 10 + x % 10;
+        x /= 10;
+    }
+    
+    return res;
+}
+
+int main() {
+    ll N, K;
+    cin >> N >> K;
+
+    ll a = f(K);
+    if (K > a) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    set<ll> st;
+    while (K <= N)
+    {
+        st.insert(K);
+        K *= 10;
+    }
+    while (a <= N)
+    {
+        st.insert(a);
+        a *= 10;
+    }
+
+    cout << st.size() << endl;
+}
