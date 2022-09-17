87d190e53cccffdd68356afd62e50b9ca313c2ac
Yuulis <yuulis2005@gmail.com>
2022-09-17 23:13:26 +0900

    [Added] ABC-269-A, B,. C, D

---
+++ AtCoder/ABC/261~280/269/D.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define all(x) (x).begin(), (x).end()
+#define fix(x) fixed << setprecision(x)
+#define rep(i,start,end) for(int i=(start);i<(int)(end);i++)
+#define rrep(i,start,end) for(int i=((int)(start));i>=(end);i--)
+
+constexpr auto PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using lld = long double;
+using mint = modint1000000007;
+// using mint = modint998244353;
+using Pair = pair<int, int>;
+
+mt19937 mt{ random_device{}() };
+uniform_int_distribution<int> rd(0, 3);
+
+ll ceil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+mint modPow(ll x, ll n) { mint ans = 1; for (ll i = 0; i < n; i++) ans *= x; return ans; }
+ll gcd(ll x, ll y) { if (x < y) swap(x, y); ll r; while (y > 0) { r = x % y; x = y; y = r; } return x; }
+ll lcm(ll x, ll y) { return ll(x / gcd(x, y)) * y; }
+ll nCk(ll N, ll K) { if (K < 0 || N < K) return 0; ll ans = 1; for (ll i = 1; i <= K; i++) { ans *= N--; ans /= i; } return ans; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+template <class T1, class T2> inline auto mod(T1 x, T2 r) { return (x % r + r) % r; }
+
+// ======================================== //
+
+int dx[6] = { -1, -1, 0, 0, 1, 1 };
+int dy[6] = { -1, 0, -1, 1, 0, 1 };
+
+int memo[2020][2020] = { 0 };
+
+int main() {
+    int N;
+    cin >> N;
+
+    dsu uf(N + 1);
+    vector<int> X(N + 1), Y(N + 1);
+    rep(i, 1, N + 1) {
+        cin >> X[i] >> Y[i];
+        X[i] += 1010;
+        Y[i] += 1010;
+        memo[X[i]][Y[i]] = i;
+    }
+
+    rep(i, 1, N + 1) {
+        rep(j, 0, 6) {
+            int nx = X[i] + dx[j];
+            int ny = Y[i] + dy[j];
+
+            if (memo[nx][ny] > 0) {
+                uf.merge(i, memo[nx][ny]);
+            }
+        }
+    }
+
+    int ans = 0;
+    rep(i, 1, N + 1) {
+        if (uf.leader(i) == i) ans++;
+    }
+
+    cout << ans << endl;
+}
