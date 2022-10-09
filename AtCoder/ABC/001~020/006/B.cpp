affa96d478047c19ac7b9bc7c0543dddf31a9ccb
Yuulis <yuulis2005@gmail.com>
2022-10-10 00:49:15 +0900

    [Added] ABC-004~010-B

---
+++ AtCoder/ABC/001~020/006/B.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define all(x) (x).begin(), (x).end()
+#define fix(x) fixed << setprecision(x)
+#define rep(i,start,end) for(int i=(start);i<(int)(end);i++)
+#define rrep(i,start,end) for(int i=((int)(start));i>=(end);i--)
+
+constexpr auto PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using lld = long double;
+using mint = modint1000000007;
+// using mint = modint998244353;
+struct Edge { int to; int cost; };
+using Pair = pair<int, int>;
+using Graph = vector<vector<Edge>>;
+
+mt19937 mt{ random_device{}() };
+uniform_int_distribution<int> rd(0, 3);
+
+ll ceil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+mint modPow(ll x, ll n) { mint ans = 1; for (ll i = 0; i < n; i++) ans *= x; return ans; }
+ll gcd(ll x, ll y) { if (x < y) swap(x, y); ll r; while (y > 0) { r = x % y; x = y; y = r; } return x; }
+ll lcm(ll x, ll y) { return ll(x / gcd(x, y)) * y; }
+ll nCk(ll N, ll K) { if (K < 0 || N < K) return 0; ll ans = 1; for (ll i = 1; i <= K; i++) { ans *= N--; ans /= i; } return ans; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+template <class T1, class T2> inline auto mod(T1 x, T2 r) { return (x % r + r) % r; }
+
+// ======================================== //
+
+int MOD = 10007;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<ll> num(1000000);
+    num[0] = 0;
+    num[1] = 0;
+    num[2] = 1;
+    rep(i, 3, n) num[i] = (num[i - 1] + num[i - 2] + num[i - 3]) % MOD;
+
+    cout << num[n - 1] << endl;
+}
