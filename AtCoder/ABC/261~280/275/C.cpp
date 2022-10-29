7e5a3e3d3bc4f859a545d3ef448b3bd6f21c0c73
Yuulis <yuulis2005@gmail.com>
2022-10-29 22:56:48 +0900

    [Added] ABC-275-A, B, C, D

---
+++ AtCoder/ABC/261~280/275/C.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define all(x) (x).begin(), (x).end()
+#define fix(x) fixed << setprecision(x)
+#define rep(i,start,end) for(int i=(start);i<(int)(end);i++)
+#define rrep(i,start,end) for(int i=((int)(start));i>=(end);i--)
+
+constexpr auto PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using lld = long double;
+using mint = modint1000000007;
+// using mint = modint998244353;
+struct Edge { int to; int cost; };
+using Pair_int = pair<int, int>;
+using Pair_ll = pair<ll, ll>;
+using Graph = vector<vector<Edge>>;
+
+mt19937 mt{ random_device{}() };
+uniform_int_distribution<int> rd(0, 3);
+
+ll ceil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+mint modPow(ll x, ll n) { mint ans = 1; for (ll i = 0; i < n; i++) ans *= x; return ans; }
+ll gcd(ll x, ll y) { if (x < y) swap(x, y); ll r; while (y > 0) { r = x % y; x = y; y = r; } return x; }
+ll lcm(ll x, ll y) { return ll(x / gcd(x, y)) * y; }
+ll nCk(ll N, ll K) { if (K < 0 || N < K) return 0; ll ans = 1; for (ll i = 1; i <= K; i++) { ans *= N--; ans /= i; } return ans; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+template <class T1, class T2> inline auto mod(T1 x, T2 r) { return (x % r + r) % r; }
+
+// ======================================== //
+
+bool check(vector<int>& x, vector<int>& y) {
+    vector<int> v;
+    for (int i = 0; i < 4; i++)
+        for (int j = i + 1; j < 4; j++) {
+            int dx = x[i] - x[j], dy = y[i] - y[j];
+            v.push_back(dx * dx + dy * dy);
+        }
+    sort(v.begin(), v.end());
+    int l = v[0];
+    if (l == 0) return false;
+    return v[0] == l && v[1] == l && v[2] == l && v[3] == l && v[4] == l * 2 && v[5] == l * 2;
+}
+
+int main() {
+    vector<Pair_int> point;
+    rep(i, 0, 9) {
+        string S;
+        cin >> S;
+        rep(j, 0, 9) if (S[j] == '#') point.push_back(make_pair(i, j));
+    }
+
+    int size = point.size();
+    if (size < 4) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    int ans = 0;
+    rep(i, 0, size) rep(j, i + 1, size) rep(k, j + 1, size) rep(l, k + 1, size) {
+        vector<int> x, y;
+        x.push_back(point[i].first); y.push_back(point[i].second);
+        x.push_back(point[j].first); y.push_back(point[j].second);
+        x.push_back(point[k].first); y.push_back(point[k].second);
+        x.push_back(point[l].first); y.push_back(point[l].second);
+
+        if (check(x, y)) ans++;
+    }
+
+    cout << ans << endl;
+}
