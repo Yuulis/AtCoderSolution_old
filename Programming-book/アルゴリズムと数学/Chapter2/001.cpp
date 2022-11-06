72cb37cd13e4f5291d8b7b7578b6423114885561
Yuulis <yuulis2005@gmail.com>
2022-11-06 20:02:01 +0900

    [Added] アルゴリズムと数学-Chapter2-001~004

---
+++ Programming-book/アルゴリズムと数学/Chapter2/001.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define all(x) (x).begin(), (x).end()
+#define fix(x) fixed << setprecision(x)
+#define rep(i,start,end) for(int i=(start);i<(int)(end);i++)
+#define rrep(i,start,end) for(int i=((int)(start));i>=(end);i--)
+
+constexpr auto PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using lld = long double;
+using mint = modint1000000007;
+// using mint = modint998244353;
+struct Edge { int to; int cost; };
+using Pair_int = pair<int, int>;
+using Pair_ll = pair<ll, ll>;
+using Graph = vector<vector<Edge>>;
+
+random_device rd;
+default_random_engine eng(rd());
+uniform_int_distribution<ll> range(-10000, 10000);
+
+ll ceil(ll a, ll b) { if (a % b == 0) return a / b; return (a / b) + 1; }
+mint modPow(ll x, ll n) { mint ans = 1; for (ll i = 0; i < n; i++) ans *= x; return ans; }
+ll gcd(ll x, ll y) { if (x < y) swap(x, y); ll r; while (y > 0) { r = x % y; x = y; y = r; } return x; }
+ll lcm(ll x, ll y) { return ll(x / gcd(x, y)) * y; }
+ll nCk(ll N, ll K) { if (K < 0 || N < K) return 0; ll ans = 1; for (ll i = 1; i <= K; i++) { ans *= N--; ans /= i; } return ans; }
+template <class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template <class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+template <class T1, class T2> inline auto mod(T1 x, T2 r) { return (x % r + r) % r; }
+
+// ======================================== //
+
+int main() {
+    int N;
+    cin >> N;
+
+    cout << 5 + N << endl;
+}
