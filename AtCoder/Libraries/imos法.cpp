9b9d5b06766a46919a3a9f036d6d189a6cabd299
Yuulis <yuulis2005@gmail.com>
2022-11-15 20:17:57 +0900

    [Added] 鉄則本-A07, B07

---
+++ AtCoder/Libraries/imos法.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define rep(i,start,end) for(int i=(start);i<(int)(end);i++)
+
+using namespace std;
+
+// ======================================== //
+
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+
+    // クエリ処理
+    vector<int> v(N + 1);
+    rep(i, 0, Q) {
+        int L, R;
+        cin >> L >> R;
+
+        v[L]++;
+        v[R + 1]--;
+    }
+
+    // 累積和計算
+    vector<int> cs(N + 2, 0);
+    rep(i, 0, N + 2) cs[i + 1] = cs[i] + v[i];
+}
